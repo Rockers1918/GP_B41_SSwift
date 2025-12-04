abcaa3eddb058251f056c13b718cb6f33660a5f2
Kingshuk Paul <77777400+kingshukpaul05@users.noreply.github.com>
2025-12-04 16:04:39 +0000

    Update main.java
    show level and lives, exit every 5 rounds

--- GB41_Simon_Swift/src/main/java/main.java
+++ GB41_Simon_Swift/src/main/java/main.java
@@ -8,52 +8,53 @@ import java.util.Scanner;
 // =========================
 // MAIN CLASS
 // =========================
-public class Main {
+public class main {
 
-    public static SwiftBotAPI swiftBot = SwiftBotAPI.INSTANCE;
+	public static SwiftBotAPI swiftBot = SwiftBotAPI.INSTANCE;
 
-    public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("file.encoding", "UTF-8");
-        CLI ui = new CLI();
+		System.setProperty("file.encoding", "UTF-8");
+		CLI ui = new CLI();
 
-        boolean running = true;
+		boolean running = true;
 
-        while (running) {
-            int choice = ui.showMenu();
+		while (running) {
+			int choice = ui.showMenu();
 
-            switch (choice) {
-                case 1: // Play
-                    swiftBot.fillUnderlights(Display.blank);
+			switch (choice) {
+			case 1: // Play
+				swiftBot.fillUnderlights(Display.blank);
 
-                    GameLogic game = new GameLogic(swiftBot, ui);
-                    game.start();
+				GameLogic game = new GameLogic(swiftBot, ui);
+				game.start();
 
-                    swiftBot.fillUnderlights(Display.blank);
-                    ui.finalScore(game.getLevel() - 1);
-                    break;
+				swiftBot.fillUnderlights(Display.blank);
+				ui.finalScore(game.getLevel() - 1);
+				break;
 
-                case 2: // Scoreboard
-                    ui.scoreboardNotImplemented();
-                    Thread.sleep(1000);
-                    break;
+			case 2: // Scoreboard
+				ui.scoreboardNotImplemented();
+				Thread.sleep(1000);
+				break;
 
-                case 3: // Settings
-                    ui.settingsNotImplemented();
-                    Thread.sleep(1000);
-                    break;
+			case 3: // Settings
+				ui.settingsNotImplemented();
+				Thread.sleep(1000);
+				break;
 
-                case 4: // Quit
-                    ui.goodbye();
-                    running = false;
-                    break;
+			case 4: // Quit
+				ui.goodbye();
+				running = false;
+				System.exit(5);
+				break;
 
-                default:
-                    ui.invalidOption();
-                    Thread.sleep(1000);
-            }
-        }
-    }
+			default:
+				ui.invalidOption();
+				Thread.sleep(1000);
+			}
+		}
+	}
 }
 
 
@@ -63,94 +64,101 @@ public class Main {
 // =========================
 class GameLogic {
 
-    private SwiftBotAPI swiftBot;
-    private CLI ui;
+	private SwiftBotAPI swiftBot;
+	private CLI ui;
 
-    private ArrayList<Integer> colours = new ArrayList<>();
-    private Random random = new Random();
-    private int lives = 3;
+	private ArrayList<Integer> colours = new ArrayList<>();
+	private Random random = new Random();
+	private int lives = 3;
 
-    private int level = 1;
-    private volatile boolean play = true;
-    private volatile boolean incomplete;
+	private int level = 1;
+	private volatile boolean play = true;
+	private volatile boolean incomplete;
 
-    public GameLogic(SwiftBotAPI bot, CLI cli) {
-        this.swiftBot = bot;
-        this.ui = cli;
-    }
+	public GameLogic(SwiftBotAPI bot, CLI cli) {
+		this.swiftBot = bot;
+		this.ui = cli;
+	}
 
-    public int getLevel() { return level; }
+	public int getLevel() { return level; }
 
-    public void start() throws InterruptedException {
+	public void start() throws InterruptedException {
 
-        incomplete = false;
+		incomplete = false;
 
-        while (play) {
+		while (play) {
 
-            ui.showLevel(level);
-            ui.showLives(lives);
+			ui.showLevelandLives(level,lives);
 
-            colours.add(random.nextInt(4));
-            Display.showSequence(swiftBot, colours);
+			colours.add(random.nextInt(4));
+			Display.showSequence(swiftBot, colours);
 
-            for (int i = 0; i < level; i++) {
+			for (int i = 0; i < level; i++) {
 
-                int expected = colours.get(i);
-                incomplete = true;
+				int expected = colours.get(i);
+				incomplete = true;
 
-                long end = System.currentTimeMillis() + 10_000;
+				long end = System.currentTimeMillis() + 10_000;
 
-                swiftBot.enableButton(Button.A, () -> handleInput(0, expected));
-                swiftBot.enableButton(Button.B, () -> handleInput(1, expected));
-                swiftBot.enableButton(Button.X, () -> handleInput(2, expected));
-                swiftBot.enableButton(Button.Y, () -> handleInput(3, expected));
+				swiftBot.enableButton(Button.A, () -> handleInput(0, expected));
+				swiftBot.enableButton(Button.B, () -> handleInput(1, expected));
+				swiftBot.enableButton(Button.X, () -> handleInput(2, expected));
+				swiftBot.enableButton(Button.Y, () -> handleInput(3, expected));
 
-                while (System.currentTimeMillis() < end && incomplete) {
-                    Thread.sleep(10);
-                }
+				while (System.currentTimeMillis() < end && incomplete) {
+					Thread.sleep(10);
+				}
 
-                swiftBot.disableAllButtons();
+				swiftBot.disableAllButtons();
 
-                if (incomplete && play) {
-                    ui.tooSlow();
-                    play = false;
-                }
-            }
+				if (incomplete && play) {
+					ui.tooSlow();
+					play = false;
+				}
+			}
 
-            if (play) {
-                level++;
-                ui.correctRound();
-                Thread.sleep(1000);
-            }
+			if (level%5==0 && level>0) {
+				if (!ui.askContinue()) {
+					play = false;
+				}
+			}
 
-            if (level % 5 == 0 && play) {
-                if (!ui.askContinue()) {
-                    play = false;
-                }
-            }
+			if (play) {
+				level++;
+				ui.correctRound();
+				Thread.sleep(1000);
+			}
 
-            if (level % 5 == 0) {
-                lives++;
-                ui.extralife(lives);
-            }
-        }
-    }
+		}
+		if (play) {
+			level++;
+			System.out.println("Correct! Next Round");
+			Thread.sleep(1000);
+		}
 
-    private void handleInput(int pressed, int expected) {
-        if (!incomplete) return;
 
-        if (pressed == expected) {
-            incomplete = false;
-        } else {
-            lives--;
-            ui.wrongButton(lives);
+		if (level % 5 == 0) {
+			lives++;
+			ui.extralife(lives);
+		}
+	}
 
-            if (lives <= 0) {
-                play = false;
-            }
-            incomplete = false;
-        }
-    }
+
+	private void handleInput(int pressed, int expected) {
+		if (!incomplete) return;
+
+		if (pressed == expected) {
+			incomplete = false;
+		} else {
+			lives--;
+			ui.wrongButton(lives);
+
+			if (lives <= 0) {
+				play = false;
+			}
+			incomplete = false;
+		}
+	}
 }
 
 
@@ -160,52 +168,52 @@ class GameLogic {
 // =========================
 class Display {
 
-    public static final int[] red = {255, 0, 0};
-    public static final int[] green = {0, 255, 0};
-    public static final int[] blue = {0, 0, 255};
-    public static final int[] yellow = {255, 255, 0};
-    public static final int[] blank = {0, 0, 0};
+	public static final int[] red = {255, 0, 0};
+	public static final int[] green = {0, 255, 0};
+	public static final int[] blue = {0, 0, 255};
+	public static final int[] yellow = {255, 255, 0};
+	public static final int[] blank = {0, 0, 0};
 
-    public static void showSequence(SwiftBotAPI swiftBot, ArrayList<Integer> colours)
-            throws InterruptedException {
+	public static void showSequence(SwiftBotAPI swiftBot, ArrayList<Integer> colours)
+			throws InterruptedException {
 
-        for (int c : colours) {
-            switch (c) {
-                case 0:
-                    swiftBot.setButtonLight(Button.A, true);
-                    light(swiftBot, Underlight.MIDDLE_LEFT, red);
-                    swiftBot.setButtonLight(Button.A, false);
-                    break;
+		for (int c : colours) {
+			switch (c) {
+			case 0:
+				swiftBot.setButtonLight(Button.A, true);
+				light(swiftBot, Underlight.MIDDLE_LEFT, red);
+				swiftBot.setButtonLight(Button.A, false);
+				break;
 
-                case 1:
-                    swiftBot.setButtonLight(Button.B, true);
-                    light(swiftBot, Underlight.BACK_LEFT, blue);
-                    swiftBot.setButtonLight(Button.B, false);
-                    break;
+			case 1:
+				swiftBot.setButtonLight(Button.B, true);
+				light(swiftBot, Underlight.BACK_LEFT, blue);
+				swiftBot.setButtonLight(Button.B, false);
+				break;
 
-                case 2:
-                    swiftBot.setButtonLight(Button.X, true);
-                    light(swiftBot, Underlight.MIDDLE_RIGHT, green);
-                    swiftBot.setButtonLight(Button.X, false);
-                    break;
+			case 2:
+				swiftBot.setButtonLight(Button.X, true);
+				light(swiftBot, Underlight.MIDDLE_RIGHT, green);
+				swiftBot.setButtonLight(Button.X, false);
+				break;
 
-                case 3:
-                    swiftBot.setButtonLight(Button.Y, true);
-                    light(swiftBot, Underlight.BACK_RIGHT, yellow);
-                    swiftBot.setButtonLight(Button.Y, false);
-                    break;
-            }
-        }
-    }
+			case 3:
+				swiftBot.setButtonLight(Button.Y, true);
+				light(swiftBot, Underlight.BACK_RIGHT, yellow);
+				swiftBot.setButtonLight(Button.Y, false);
+				break;
+			}
+		}
+	}
 
-    private static void light(SwiftBotAPI bot, Underlight u, int[] col)
-            throws InterruptedException {
+	private static void light(SwiftBotAPI bot, Underlight u, int[] col)
+			throws InterruptedException {
 
-        bot.setUnderlight(u, col);
-        Thread.sleep(800);
-        bot.setUnderlight(u, blank);
-        Thread.sleep(200);
-    }
+		bot.setUnderlight(u, col);
+		Thread.sleep(700);
+		bot.setUnderlight(u, blank);
+		Thread.sleep(200);
+	}
 }
 
 
@@ -215,98 +223,93 @@ class Display {
 // =========================
 class CLI {
 
-    private Scanner scanner = new Scanner(System.in);
+	private Scanner scanner = new Scanner(System.in);
 
-    public int showMenu() {
-        System.out.println("========================================");
-        System.out.println("         ____  _                         ");
-        System.out.println("        / ___|(_)_ __ ___   ___ _ __     ");
-        System.out.println("        \\___ \\| | '_ ` _ \\ /   \\ '_ \\ ");
-        System.out.println("         ___) | | | | | | |  |  || | |   ");
-        System.out.println("        |____/|_|_| |_| |_|\\___/_| |_|   ");
-        System.out.println();
-        System.out.println("           S I M O N   S W I F T          ");
-        System.out.println("========================================");
-        System.out.println("              1) Play");
-        System.out.println("              2) Scoreboard");
-        System.out.println("              3) Settings");
-        System.out.println("              4) Quit");
-        System.out.println("========================================");
-        System.out.print("Select an option: ");
+	public int showMenu() {
+		System.out.println("========================================");
+		System.out.println("         ____  _                         ");
+		System.out.println("        / ___|(_)_ __ ___   ___ _ __     ");
+		System.out.println("        \\___ \\| | '_ ` _ \\ /   \\ '_ \\ ");
+		System.out.println("         ___) | | | | | | |  |  || | |   ");
+		System.out.println("        |____/|_|_| |_| |_|\\___/_| |_|   ");
+		System.out.println();
+		System.out.println("           S I M O N   S W I F T          ");
+		System.out.println("========================================");
+		System.out.println("              1) Play");
+		System.out.println("              2) Scoreboard");
+		System.out.println("              3) Settings");
+		System.out.println("              4) Quit");
+		System.out.println("========================================");
+		System.out.print("Select an option: ");
 
-        int choice = -1;
-        if (scanner.hasNextInt()) {
-            choice = scanner.nextInt();
-            scanner.nextLine();
-        }
-        return choice;
-    }
+		int choice = -1;
+		if (scanner.hasNextInt()) {
+			choice = scanner.nextInt();
+			scanner.nextLine();
+		}
+		return choice;
+	}
 
-    public void showWelcome() {
-        System.out.println("\n========================================");
-        System.out.println("         SWIFTBOT SIMON SAYS            ");
-        System.out.println("========================================");
-        System.out.println("Press ENTER to begin...");
-        scanner.nextLine();
-    }
+	public void showWelcome() {
+		System.out.println("\n========================================");
+		System.out.println("         SWIFTBOT SIMON SAYS            ");
+		System.out.println("========================================");
+		System.out.println("Press ENTER to begin...");
+		scanner.nextLine();
+	}
 
-    public void showLevel(int level) {
-        System.out.println("\n========================================");
-        System.out.println("              LEVEL " + level + "               ");
-        System.out.println("========================================");
-    }
+	public void showLevelandLives(int level, int lives) {
+		System.out.println("\n========================================");
+		System.out.println("    LEVEL " + level + "          LIVES: " + lives);
+		System.out.println("========================================");
+	}
 
-    public void showLives(int lives) {
-        System.out.println("\n========================================");
-        System.out.println("              LIVES " + lives + "               ");
-        System.out.println("========================================");
-    }
 
-    public void extralife(int currentlives) {
-        System.out.println("\n========================================");
-        System.out.println("EXTRA LIFE!");
-        System.out.println("Lives remaining: " + currentlives);
-        System.out.println("========================================");
-    }
+	public void extralife(int currentlives) {
+		System.out.println("\n========================================");
+		System.out.println("EXTRA LIFE!");
+		System.out.println("Lives remaining: " + currentlives);
+		System.out.println("========================================");
+	}
 
-    public void correctRound() {
-        System.out.println("Correct! Moving to next round...");
-    }
+	public void correctRound() {
+		System.out.println("Correct! Moving to next round...");
+	}
 
-    public void tooSlow() {
-        System.out.println("Too slow! Game over.");
-    }
+	public void tooSlow() {
+		System.out.println("Too slow! Game over.");
+	}
 
-    public void wrongButton(int remainingLives) {
-        System.out.println("Wrong button! Lives remaining: " + remainingLives);
-    }
+	public void wrongButton(int remainingLives) {
+		System.out.println("Wrong button! Lives remaining: " + remainingLives);
+	}
 
-    public boolean askContinue() {
-        System.out.print("Continue? (y/n): ");
-        String ans = scanner.nextLine();
-        return ans.equalsIgnoreCase("y");
-    }
+	public boolean askContinue() {
+		System.out.print("Continue? (y/n): ");
+		String ans = scanner.nextLine();
+		return ans.equalsIgnoreCase("y");
+	}
 
-    public void finalScore(int score) {
-        System.out.println("\n========================================");
-        System.out.println("               GAME OVER                ");
-        System.out.println("           Final Score: " + score + "           ");
-        System.out.println("========================================\n");
-    }
+	public void finalScore(int score) {
+		System.out.println("\n========================================");
+		System.out.println("               GAME OVER                ");
+		System.out.println("           Final Score: " + score + "           ");
+		System.out.println("========================================\n");
+	}
 
-    public void settingsNotImplemented() {
-        System.out.println("Settings not implemented yet!");
-    }
+	public void settingsNotImplemented() {
+		System.out.println("Settings not implemented yet!");
+	}
 
-    public void scoreboardNotImplemented() {
-        System.out.println("Scoreboard not implemented yet!");
-    }
+	public void scoreboardNotImplemented() {
+		System.out.println("Scoreboard not implemented yet!");
+	}
 
-    public void goodbye() {
-        System.out.println("Goodbye!");
-    }
+	public void goodbye() {
+		System.out.println("Goodbye!");
+	}
 
-    public void invalidOption() {
-        System.out.println("Invalid option! Try again.");
-    }
+	public void invalidOption() {
+		System.out.println("Invalid option! Try again.");
+	}
 }
